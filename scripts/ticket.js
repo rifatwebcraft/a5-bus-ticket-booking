872218c3f99da99ff0a5793387faaaf025caa111
rifatwebcraft <cmohammadrifat@gmail.com>
2024-02-21 00:07:56 +0600

    my work progres slowly

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -3,12 +3,20 @@ let count = 0;
 let availableSeat = 8;
 for (const seat of allSeat){
     seat.addEventListener('click', function(e){
-
+        if(seat.classList.contains("bg-green-400") === true){
+            return
+        }
+        if(count >= 4){
+            alert('Soory! You cannot book more than four seats!')
+            return
+        }
         seat.classList.add('bg-green-400');
         count = count + 1;
         // console.log(count);
         // document.getElementById('seatBookingDisplay').innerText = count;
         setInnerText('seatBookingDisplay', count);
+
+
 
         availableSeat = availableSeat-1;
         // document.getElementById('available-seat').innerText = availableSeat;
@@ -16,6 +24,7 @@ for (const seat of allSeat){
 
         const seatName = e.target.innerText;
         console.log(seatName);
+        
 
         const selectedContainer = document.getElementById('seat-booking-details');
 
@@ -32,11 +41,38 @@ for (const seat of allSeat){
         tr.appendChild(td2);
         selectedContainer.appendChild(tr);
 
-        
+
+        totalPrice('total-price', parseInt(td2.innerText));
+        // grandTotalPrice('grand-total', parseInt(td2.innerText));
+        grandTotal();   
+   
     })
 }
 
+function totalPrice (id, value){
+    const totalCost = document.getElementById(id).innerText;
+    const convertTotal = parseInt(totalCost);
+    const convertSingleCost = value;
+    const sumTotal = convertTotal + convertSingleCost;
+   setInnerText(id, sumTotal);
+}
+function grandTotalPrice (id, value){
+    const totalCost = document.getElementById(id).innerText;
+    console.log(totalCost);
+    const convertTotal = parseInt(totalCost);
+    const convertSingleCost = value;
+    const sumTotal = convertTotal + convertSingleCost;
+   setInnerText(id, sumTotal);
+}
 
+function grandTotal(){
+    const totalCost = document.getElementById('total-price').innerText;
+    const convertTotalCost = parseInt(totalCost);
+
+
+
+    setInnerText('grand-total', convertTotalCost);
+}
 function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 }
